39a124c552e0a190e51b3540ffc195c9fae44e93
meenakshisureshbabu <meenakshi.bca@gmail.com>
2023-12-14 15:04:45 -0500

    Connected the data and can see my json in the browser

---
+++ models/pokemon.js
@@ -0,0 +1,12 @@
+const pokemon = [
+    {name: "bulbasaur", img: "https://www.shutterstock.com/image-vector/bulbasaur-pokemon-anime-cartoon-character-600nw-2335225105.jpg"},
+    {name: "ivysaur", img: "https://marriland.com/wp-content/plugins/marriland-core/images/pokemon/sprites/home/full/ivysaur.png"},
+    {name: "venusaur", img: "https://static.pokemonpets.com/images/monsters-images-800-800/3-Venusaur.webp"},
+    {name: "charmander", img: "https://www.giantbomb.com/a/uploads/original/0/6087/2438704-1202149925_t.png"},
+    {name: "charizard", img: "https://static.wikia.nocookie.net/sonicpokemon/images/b/bf/Charizard_AG_anime.png/revision/latest?cb=20130714192025"},
+    {name: "squirtle", img: "https://static.pokemonpets.com/images/monsters-images-800-800/7-Squirtle.webp"},
+    {name: "wartortle", img: "https://static.wikia.nocookie.net/legendsofthemultiuniverse/images/2/2d/8-Wartortle.png/revision/latest?cb=20190204023340"}
+ ];
+
+
+ module.exports = pokemon
--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require("express")
 
 const app = express()
 
+const pokemondata = require('./models/pokemon.js')
+
 
 app.get('/',(req,res) => {
-    res.send("<h1>WELCOME TO THE POKEMON APP</h1>")
+    res.send("<a href=/pokemon><h1>WELCOME TO THE POKEMON APP</h1></a>")
 })
 
 
 app.get('/pokemon',(req,res) => {
-    res.render("Index")
+    res.send(pokemondata)
 })
+
+
+
 
 
 
